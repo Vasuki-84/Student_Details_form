ba5e671fa14eb3578c48dc8202c31af319c0e797
Vasuki t <iamvasukithangaraj@gmail.com>
2025-11-08 16:40:48 +0530

    added routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import StudentTable from "./Pages/StudentTable";
 import Home from "./Components/Home";
 import CreateStudent from "./Pages/CreateStudent";
+import EditStudent from "./Pages/EditStudent";
+import ViewDetails from "./Pages/ViewDetails";
 
 function App() {
   return (
     <div>
-      <Home />
+     
       <Routes>
+        <Route path="/" element={ <Home />}/>
         <Route path="/studentTable" element={<StudentTable />} />
         <Route path="/createStudent" element={<CreateStudent />} />
+        <Route path="/editStudent/:studentid" element={<EditStudent />} />
+        <Route path="/ViewDetails/:studentid" element={<ViewDetails />} />
       </Routes>
     </div>
   );
