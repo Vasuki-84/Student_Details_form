3c8935cd3ca6be87cdad9b5188e1aaf63deb709a
Vasuki t <iamvasukithangaraj@gmail.com>
2025-11-08 17:34:07 +0530

    added json-server

--- src/Pages/StudentTable.jsx
+++ src/Pages/StudentTable.jsx
@@ -6,7 +6,7 @@ function StudentTable() {
     <div className=" mt-10 border rounded-sm mx-3 py-3 shadow-lg bg-gray-100">
       <h2 className="font-bold text-center mb-5">Student Records</h2>
       <div className="">
-        <Link className="btn bg-blue-300 py-2 px-2 rounded-lg mx-2 mb-5 mt-4">
+        <Link to="/createStudent" className="btn bg-blue-300 py-2 px-2 rounded-lg mx-2 mb-5 mt-4">
           Add new student
         </Link>
         <table className="border-collapse mt-4 mx-10 border border-gray-400 mx-auto ...">
@@ -30,8 +30,8 @@ function StudentTable() {
                 45457983344
               </td>
               <td className="border  px-5 py-2 border-gray-600 ...">
-                <Link className="btn bg-yellow-300 mx-3 px-2 py-1 rounded-sm">View</Link>
-                <Link className="btn bg-gray-500 mx-3 px-2 py-1 rounded-sm">Edit</Link>
+                <Link to="/ViewDetails/:studentid" className="btn bg-yellow-300 mx-3 px-2 py-1 rounded-sm">View</Link>
+                <Link to="/editStudent/:studentid" className="btn bg-gray-500 mx-3 px-2 py-1 rounded-sm">Edit</Link>
                 <Link className="btn bg-red-500 mx-3 px-2 py-1 rounded-sm"> Delete</Link>
               </td>
             </tr>
